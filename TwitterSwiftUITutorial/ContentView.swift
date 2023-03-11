4d7d15f80659d4f64121b2e21ff5bbbf977f82d2
chavalk <chavalk@hotmail.com>
2023-03-11 08:20:28 -0600

    Add tab view and the needed tab items.

--- TwitterSwiftUITutorial/ContentView.swift
+++ TwitterSwiftUITutorial/ContentView.swift
@@ -11,6 +11,25 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             
+            TabView {
+                Text("Feed")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                
+                Text("Message")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
+            }
         }
     }
 }
