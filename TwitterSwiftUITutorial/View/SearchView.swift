81397fd25b6d801fdab39211479782e0a61637eb
chavalk <chavalk@hotmail.com>
2023-03-11 11:13:38 -0600

    Add search view file.

---
+++ TwitterSwiftUITutorial/View/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/11/23.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("My search view")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
