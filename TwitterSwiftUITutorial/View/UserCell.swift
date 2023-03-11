78c8f709ad2d28a58b71cf7e47d51b4047874301
chavalk <chavalk@hotmail.com>
2023-03-11 12:24:09 -0600

    Add user cell view.

---
+++ TwitterSwiftUITutorial/View/UserCell.swift
@@ -0,0 +1,20 @@
+//
+//  UserCell.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/11/23.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserCell_Previews: PreviewProvider {
+    static var previews: some View {
+        UserCell()
+    }
+}
