a3e71231283546d3b0515c5f8433d6de369cf45c
chavalk <chavalk@hotmail.com>
2023-03-11 12:11:51 -0600

    Move code placed in search view in error to search bar view.

--- TwitterSwiftUITutorial/View/SearchView.swift
+++ TwitterSwiftUITutorial/View/SearchView.swift
@@ -8,30 +8,13 @@
 import SwiftUI
 
 struct SearchView: View {
-    @Binding var text: String
-    
     var body: some View {
-        HStack {
-            TextField("Search...", text: $text)
-                .padding(8)
-                .padding(.horizontal, 24)
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 8)
-                    }
-                )
-        }
-        .padding(.horizontal, 10)
+        Text("Hi")
     }
 }
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchView(text: .constant("Search..."))
+        SearchView()
     }
 }
