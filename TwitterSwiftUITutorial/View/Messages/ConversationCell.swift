4a2bacb78aaee7aca94d22a32a6d263cb0beace7
chavalk <chavalk@hotmail.com>
2023-03-11 16:00:18 -0600

    Add hstack woth profile image, username and message text to conversation cell.

--- TwitterSwiftUITutorial/View/Messages/ConversationCell.swift
+++ TwitterSwiftUITutorial/View/Messages/ConversationCell.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct ConversationCell: View {
     var body: some View {
-        Text("Conversation cell stuff goes here...")
+        HStack(spacing: 12) {
+            Image("venom-10")
+                .resizable()
+                .scaledToFill()
+                .clipped()
+                .frame(width: 56, height: 56)
+                .cornerRadius(28)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("venom")
+                    .font(.system(size: 14, weight: .semibold))
+                
+                Text("Longer message text to see what happens when I do this.")
+                    .font(.system(size: 14))
+            }
+            .padding(.trailing)
+        }
     }
 }
 
