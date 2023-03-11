04a9d6e29468156c2ff846681efb8a8e41bd91c8
chavalk <chavalk@hotmail.com>
2023-03-10 21:46:19 -0600

    First commit.

---
+++ TwitterSwiftUITutorial/TwitterSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUITutorialApp.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
