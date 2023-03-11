67516cae12ce8aada377dbc59ce34da016014e61
chavalk <chavalk@hotmail.com>
2023-03-11 16:05:47 -0600

    Embed hstack in vstack in order to add divider at the bottom of conversation cell.

--- TwitterSwiftUITutorial/View/Messages/ConversationCell.swift
+++ TwitterSwiftUITutorial/View/Messages/ConversationCell.swift
@@ -9,22 +9,26 @@ import SwiftUI
 
 struct ConversationCell: View {
     var body: some View {
-        HStack(spacing: 12) {
-            Image("venom-10")
-                .resizable()
-                .scaledToFill()
-                .clipped()
-                .frame(width: 56, height: 56)
-                .cornerRadius(28)
-            
-            VStack(alignment: .leading, spacing: 4) {
-                Text("venom")
-                    .font(.system(size: 14, weight: .semibold))
+        VStack {
+            HStack(spacing: 12) {
+                Image("venom-10")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(28)
                 
-                Text("Longer message text to see what happens when I do this.")
-                    .font(.system(size: 14))
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("venom")
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text("Longer message text to see what happens when I do this.")
+                        .font(.system(size: 14))
+                }
+                .padding(.trailing)
             }
-            .padding(.trailing)
+            
+            Divider()
         }
     }
 }
