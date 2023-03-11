61edbcb295c715d5951d849aa5254c7fc225ff01
chavalk <chavalk@hotmail.com>
2023-03-11 15:46:19 -0600

    Add conversation cell.

---
+++ TwitterSwiftUITutorial/View/Messages/ConversationCell.swift
@@ -0,0 +1,20 @@
+//
+//  ConversationCell.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/11/23.
+//
+
+import SwiftUI
+
+struct ConversationCell: View {
+    var body: some View {
+        Text("Conversation cell stuff goes here...")
+    }
+}
+
+struct ConversationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationCell()
+    }
+}
