1ace1cf293dcd69e217fc81615b24de16995d085
chavalk <chavalk@hotmail.com>
2023-03-11 09:28:46 -0600

    Add tweet cell file.

---
+++ TwitterSwiftUITutorial/View/TweetCell.swift
@@ -0,0 +1,20 @@
+//
+//  TweetCell.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/11/23.
+//
+
+import SwiftUI
+
+struct TweetCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetCell()
+    }
+}
