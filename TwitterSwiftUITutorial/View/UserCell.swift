8c596de814b5485582655929ea7ffa83664fc753
chavalk <chavalk@hotmail.com>
2023-03-11 13:12:27 -0600

    Add hstack with profile image.

--- TwitterSwiftUITutorial/View/UserCell.swift
+++ TwitterSwiftUITutorial/View/UserCell.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct UserCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image("venom-10")
+                .resizable()
+                .scaledToFill()
+                .clipped()
+                .frame(width: 56, height: 56)
+                .cornerRadius(28)
+        }
     }
 }
 
