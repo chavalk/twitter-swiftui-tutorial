89f82fea88f429e0c0f8943df0a81633c91b3dc2
chavalk <chavalk@hotmail.com>
2023-03-14 17:16:11 -0400

    Add message input view file.

---
+++ TwitterSwiftUITutorial/View/Messages/MessageInputView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageInputView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/14/23.
+//
+
+import SwiftUI
+
+struct MessageInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageInputView()
+    }
+}
