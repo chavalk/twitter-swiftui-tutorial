43ab848cea746a8a5ae4c02ece274c0b6f56670b
chavalk <chavalk@hotmail.com>
2023-03-14 17:24:01 -0400

    Add hstack with text field to message input view.

--- TwitterSwiftUITutorial/View/Messages/MessageInputView.swift
+++ TwitterSwiftUITutorial/View/Messages/MessageInputView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct MessageInputView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextField("message", text: <#T##Binding<String>#>)
+        }
     }
 }
 
