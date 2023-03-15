0f1e2b0b2c4ed7e4476e6d36fe395939c582930c
chavalk <chavalk@hotmail.com>
2023-03-14 21:56:54 -0400

    Configure path for chat bubble.

--- TwitterSwiftUITutorial/View/Messages/ChatBubble.swift
+++ TwitterSwiftUITutorial/View/Messages/ChatBubble.swift
@@ -11,7 +11,9 @@ struct ChatBubble: Shape {
     var isFromCurrentUser: Bool
     
     func path(in rect: CGRect) -> Path {
-        <#code#>
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight, isFromCurrentUser ? .bottomLeft : .bottomRight], cornerRadii: CGSize(width: 16, height: 16))
+        
+        return Path(path.cgPath)
     }
 }
 
