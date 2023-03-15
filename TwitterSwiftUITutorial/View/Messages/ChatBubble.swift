2f6ec09c457da3a7f9d79e97c9330ae93f8a620d
chavalk <chavalk@hotmail.com>
2023-03-14 21:25:52 -0400

    Add chat bubble file.

---
+++ TwitterSwiftUITutorial/View/Messages/ChatBubble.swift
@@ -0,0 +1,20 @@
+//
+//  ChatBubble.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/14/23.
+//
+
+import SwiftUI
+
+struct ChatBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatBubble()
+    }
+}
