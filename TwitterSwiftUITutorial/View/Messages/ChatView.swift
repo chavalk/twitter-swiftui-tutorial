81fb3c201f0ccbc754c99541abf708a5e9a4fc13
chavalk <chavalk@hotmail.com>
2023-03-15 21:01:40 -0400

    Add if statement to determine how to display chat bubble to user.

--- TwitterSwiftUITutorial/View/Messages/ChatView.swift
+++ TwitterSwiftUITutorial/View/Messages/ChatView.swift
@@ -15,13 +15,32 @@ struct ChatView: View {
                 VStack(alignment: .leading, spacing: 12) {
                     ForEach(MOCK_MESSAGES) { message in
                         HStack {
-                            Spacer()
-                            Text(message.messageText)
-                                .padding()
-                                .background(Color.blue)
-                                .clipShape(ChatBubble(isFromCurrentUser: true))
-                                .foregroundColor(.white)
-                                .padding(.horizontal)
+                            if message.isCurrentUser {
+                                Spacer()
+                                Text(message.messageText)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .clipShape(ChatBubble(isFromCurrentUser: true))
+                                    .foregroundColor(.white)
+                                    .padding(.horizontal)
+                            } else {
+                                HStack {
+                                    Image(message.imageName)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 40, height: 40)
+                                        .clipShape(Circle())
+                                    
+                                    Text(message.messageText)
+                                        .padding()
+                                        .background(Color(.systemGray5))
+                                        .clipShape(ChatBubble(isFromCurrentUser: false))
+                                        .foregroundColor(.black)
+                                        .padding(.horizontal)
+                                    
+                                    Spacer()
+                                }
+                            }
                         }
                     }
                 }
