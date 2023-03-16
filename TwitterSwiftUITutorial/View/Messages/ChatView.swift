f8bbe4e5f66fca4c311920ae3bf3135e2a7b03ae
chavalk <chavalk@hotmail.com>
2023-03-15 23:20:21 -0400

    Add Message view file and move code from chat view.

--- TwitterSwiftUITutorial/View/Messages/ChatView.swift
+++ TwitterSwiftUITutorial/View/Messages/ChatView.swift
@@ -31,37 +31,3 @@ struct ChatView_Previews: PreviewProvider {
         ChatView()
     }
 }
-
-struct MessageView: View {
-    let message: MockMessage
-    
-    var body: some View {
-        HStack {
-            if message.isCurrentUser {
-                Spacer()
-                Text(message.messageText)
-                    .padding()
-                    .background(Color.blue)
-                    .clipShape(ChatBubble(isFromCurrentUser: true))
-                    .foregroundColor(.white)
-                    .padding(.horizontal)
-            } else {
-                HStack(alignment: .bottom) {
-                    Image(message.imageName)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 40, height: 40)
-                        .clipShape(Circle())
-                    
-                    Text(message.messageText)
-                        .padding()
-                        .background(Color(.systemGray5))
-                        .clipShape(ChatBubble(isFromCurrentUser: false))
-                        .foregroundColor(.black)
-                }
-                .padding(.horizontal)
-                Spacer()
-            }
-        }
-    }
-}
