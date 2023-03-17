ec2cad03f6ce509e31fd0235964f3ca69d683924
chavalk <chavalk@hotmail.com>
2023-03-17 15:50:20 -0500

    Add user profile view.

---
+++ TwitterSwiftUITutorial/View/Profile/UserProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfileView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/17/23.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text("User Profile")
+    }
+}
+
+struct UserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfileView()
+    }
+}
