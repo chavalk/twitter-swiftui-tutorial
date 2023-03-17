41a15173c49baa0600cccd32539f90a4ff62136e
chavalk <chavalk@hotmail.com>
2023-03-16 23:33:20 -0400

    Add new message view.

---
+++ TwitterSwiftUITutorial/View/Messages/NewMessageView.swift
@@ -0,0 +1,20 @@
+//
+//  NewMessageView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/16/23.
+//
+
+import SwiftUI
+
+struct NewMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewMessageView()
+    }
+}
