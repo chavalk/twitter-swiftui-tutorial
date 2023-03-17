5b31a2ac3793a21565f13ecfe8f422ff0b85979e
chavalk <chavalk@hotmail.com>
2023-03-16 23:35:24 -0400

    Copy body and state bar from search view to paste in new message view.

--- TwitterSwiftUITutorial/View/Messages/NewMessageView.swift
+++ TwitterSwiftUITutorial/View/Messages/NewMessageView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct NewMessageView: View {
+    @State var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            SearchBar(text: $searchText)
+                .padding()
+            
+            VStack(alignment: .leading) {
+                ForEach(0..<10) { _ in
+                    HStack { Spacer() }
+                    UserCell()
+                }
+            }
+            .padding(.leading)
+        }
     }
 }
 
