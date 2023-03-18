28a3adf5f13f23e006d34efe17047c7f6839efad
chavalk <chavalk@hotmail.com>
2023-03-17 20:01:43 -0500

    Add profile header view.

---
+++ TwitterSwiftUITutorial/View/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/17/23.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHeaderView()
+    }
+}
