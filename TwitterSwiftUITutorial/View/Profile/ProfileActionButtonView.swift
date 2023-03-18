a738dbceb8a15307d061f64ccb1cc58a1af36f10
chavalk <chavalk@hotmail.com>
2023-03-18 14:52:56 -0500

    Add if statement to determine which button to display to user.

--- TwitterSwiftUITutorial/View/Profile/ProfileActionButtonView.swift
+++ TwitterSwiftUITutorial/View/Profile/ProfileActionButtonView.swift
@@ -11,20 +11,44 @@ struct ProfileActionButtonView: View {
     let isCurrentUser: Bool
     
     var body: some View {
-        Button {
-            
-        } label: {
-            Text("Edit Profile")
-                .frame(width: 360, height: 40)
-                .background(Color.blue)
-                .foregroundColor(.white)
+        if isCurrentUser {
+            Button {
+                
+            } label: {
+                Text("Edit Profile")
+                    .frame(width: 360, height: 40)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+            }
+            .cornerRadius(20)
+        } else {
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("Follow")
+                        .frame(width: 180, height: 40)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                }
+                .cornerRadius(20)
+                
+                Button {
+                    
+                } label: {
+                    Text("Message")
+                        .frame(width: 180, height: 40)
+                        .background(Color.purple)
+                        .foregroundColor(.white)
+                }
+                .cornerRadius(20)
+            }
         }
-        .cornerRadius(20)
     }
 }
 
 struct ProfileActionButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileActionButtonView(isCurrentUser: false)
+        ProfileActionButtonView(isCurrentUser: true)
     }
 }
