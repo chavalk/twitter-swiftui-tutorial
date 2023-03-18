abc38b1e24b8e40767fe62472efcbf20be40171c
chavalk <chavalk@hotmail.com>
2023-03-18 10:23:00 -0500

    Add navigation title to user profile view.

--- TwitterSwiftUITutorial/View/Profile/UserProfileView.swift
+++ TwitterSwiftUITutorial/View/Profile/UserProfileView.swift
@@ -14,6 +14,8 @@ struct UserProfileView: View {
                 ProfileHeaderView()
                     .padding()
             }
+            
+            .navigationTitle("batman")
         }
     }
 }
