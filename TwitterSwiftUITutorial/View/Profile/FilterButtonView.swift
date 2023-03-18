2b1e2d64b46d694b7b18282e70b18f50604319b3
chavalk <chavalk@hotmail.com>
2023-03-17 20:04:47 -0500

    Add filter button view.

---
+++ TwitterSwiftUITutorial/View/Profile/FilterButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  FilterButtonView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/17/23.
+//
+
+import SwiftUI
+
+struct FilterButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterButtonView()
+    }
+}
