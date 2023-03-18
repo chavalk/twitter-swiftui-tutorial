b6327198c61df35121fb489ba14082840afc5559
chavalk <chavalk@hotmail.com>
2023-03-18 15:39:07 -0500

    Add vstack and hstack to filter button view.

--- TwitterSwiftUITutorial/View/Profile/FilterButtonView.swift
+++ TwitterSwiftUITutorial/View/Profile/FilterButtonView.swift
@@ -23,7 +23,11 @@ enum TweetFilterOptions: Int, CaseIterable {
 
 struct FilterButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                
+            }
+        }
     }
 }
 
