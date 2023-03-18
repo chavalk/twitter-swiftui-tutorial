a0ea8ffa8326865b1cef53df5a672fcf068a6ee2
chavalk <chavalk@hotmail.com>
2023-03-17 20:02:53 -0500

    Add profile action button view.

---
+++ TwitterSwiftUITutorial/View/Profile/ProfileActionButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileActionButtonView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/17/23.
+//
+
+import SwiftUI
+
+struct ProfileActionButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileActionButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileActionButtonView()
+    }
+}
