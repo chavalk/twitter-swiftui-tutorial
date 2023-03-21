35181e94d8ae33aace54f0bdb220d989f177101f
chavalk <chavalk@hotmail.com>
2023-03-21 14:36:17 -0500

    Add new tweet view file.

---
+++ TwitterSwiftUITutorial/View/Tweets/NewTweetView.swift
@@ -0,0 +1,20 @@
+//
+//  NewTweetView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/21/23.
+//
+
+import SwiftUI
+
+struct NewTweetView: View {
+    var body: some View {
+        Text("New Tweet View")
+    }
+}
+
+struct NewTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTweetView()
+    }
+}
