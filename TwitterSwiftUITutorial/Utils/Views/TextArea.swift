8f1e49f358429b261115b2631bb504fec95569f9
chavalk <chavalk@hotmail.com>
2023-03-21 16:08:32 -0500

    Add text area file.

---
+++ TwitterSwiftUITutorial/Utils/Views/TextArea.swift
@@ -0,0 +1,20 @@
+//
+//  TextArea.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/21/23.
+//
+
+import SwiftUI
+
+struct TextArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextArea_Previews: PreviewProvider {
+    static var previews: some View {
+        TextArea()
+    }
+}
