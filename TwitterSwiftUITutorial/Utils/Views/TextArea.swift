4114bf6d5314d3aa265e0d960d7d7d4d1e94506f
chavalk <chavalk@hotmail.com>
2023-03-21 16:31:53 -0500

    Add binding var text and placeholder.

--- TwitterSwiftUITutorial/Utils/Views/TextArea.swift
+++ TwitterSwiftUITutorial/Utils/Views/TextArea.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct TextArea: View {
+    @Binding var text: String
+    let placeholder: String
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
