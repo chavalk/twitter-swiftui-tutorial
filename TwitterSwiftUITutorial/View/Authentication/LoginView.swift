7a39f210a27605225e84bed515dd74558fedfe85
chavalk <chavalk@hotmail.com>
2023-03-22 08:58:44 -0500

    Embed login view in navigation view.

--- TwitterSwiftUITutorial/View/Authentication/LoginView.swift
+++ TwitterSwiftUITutorial/View/Authentication/LoginView.swift
@@ -12,72 +12,74 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("twitter-logo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 220, height: 100)
-                    .padding(.top, 88)
-                    .padding(.bottom, 32)
-                
-                VStack(spacing: 20) {
-                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
+        NavigationView {
+            ZStack {
+                VStack {
+                    Image("twitter-logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 220, height: 100)
+                        .padding(.top, 88)
+                        .padding(.bottom, 32)
                     
-                    CustomSecureField(text: $password, placeholder: Text("Password"))
-                        .padding()
-                        .background(Color(.init(white: 1, alpha: 0.15)))
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                }
-                .padding(.horizontal, 32)
-                
-                HStack {
-                    Spacer()
+                    VStack(spacing: 20) {
+                        CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                        
+                        CustomSecureField(text: $password, placeholder: Text("Password"))
+                            .padding()
+                            .background(Color(.init(white: 1, alpha: 0.15)))
+                            .cornerRadius(10)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.horizontal, 32)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Text("Forgot password?")
+                                .font(.footnote)
+                                .bold()
+                                .foregroundColor(.white)
+                                .padding(.top, 16)
+                                .padding(.trailing, 32)
+                        }
+                    }
                     
                     Button {
                         
                     } label: {
-                        Text("Forgot password?")
-                            .font(.footnote)
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding(.top, 16)
-                            .padding(.trailing, 32)
+                        Text("Sign In")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .frame(width: 360, height: 50)
+                            .background(Color.white)
+                            .clipShape(Capsule())
+                            .padding()
                     }
-                }
-                
-                Button {
-                    
-                } label: {
-                    Text("Sign In")
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .frame(width: 360, height: 50)
-                        .background(Color.white)
-                        .clipShape(Capsule())
-                        .padding()
-                }
 
-                Spacer()
-                
-                HStack {
-                    Text("Don't have an account?")
-                        .font(.system(size: 14))
+                    Spacer()
                     
-                    Text("Sign Up")
-                        .font(.system(size: 14, weight: .semibold))
+                    HStack {
+                        Text("Don't have an account?")
+                            .font(.system(size: 14))
+                        
+                        Text("Sign Up")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    .foregroundColor(.white)
+                    .padding(.bottom, 40)
                 }
-                .foregroundColor(.white)
-                .padding(.bottom, 40)
             }
-        }
-        .background(Color(red: 0.113, green: 0.632, blue: 0.954))
+            .background(Color(red: 0.113, green: 0.632, blue: 0.954))
         .ignoresSafeArea()
+        }
     }
 }
 
