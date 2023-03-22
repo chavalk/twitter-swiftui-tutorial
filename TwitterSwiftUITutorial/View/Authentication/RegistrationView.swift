5f78b1226ba8a909cd82650e6b5bfcf936e6b2d9
chavalk <chavalk@hotmail.com>
2023-03-22 07:50:17 -0500

    Copy and paste body from login view to registration view and add email and password state vars to registration view.

--- TwitterSwiftUITutorial/View/Authentication/RegistrationView.swift
+++ TwitterSwiftUITutorial/View/Authentication/RegistrationView.swift
@@ -8,8 +8,77 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Image("twitter-logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 220, height: 100)
+                    .padding(.top, 88)
+                    .padding(.bottom, 32)
+                
+                VStack(spacing: 20) {
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    
+                    CustomSecureField(text: $password, placeholder: Text("Password"))
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 32)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Forgot password?")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.top, 16)
+                            .padding(.trailing, 32)
+                    }
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .frame(width: 360, height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                        .padding()
+                }
+
+                Spacer()
+                
+                HStack {
+                    Text("Don't have an account?")
+                        .font(.system(size: 14))
+                    
+                    Text("Sign Up")
+                        .font(.system(size: 14, weight: .semibold))
+                }
+                .foregroundColor(.white)
+                .padding(.bottom, 40)
+            }
+        }
+        .background(Color(red: 0.113, green: 0.632, blue: 0.954))
+        .ignoresSafeArea()
+    }
     }
 }
 
