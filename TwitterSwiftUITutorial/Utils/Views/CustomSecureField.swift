22e901bc29d6d8c45eba35b4dd98fed40af0717f
chavalk <chavalk@hotmail.com>
2023-03-21 20:45:08 -0500

    Add custom secure field file.

---
+++ TwitterSwiftUITutorial/Utils/Views/CustomSecureField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomSecureField.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 3/21/23.
+//
+
+import SwiftUI
+
+struct CustomSecureField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomSecureField_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSecureField()
+    }
+}
