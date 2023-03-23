9554502c4aeb3bb7c2c4076974677af0c64f3b87
chavalk <chavalk@hotmail.com>
2023-03-23 09:43:42 -0500

    Add image picker extension and implement correct make coordinator function.

--- TwitterSwiftUITutorial/Utils/Views/ImagePicker.swift
+++ TwitterSwiftUITutorial/Utils/Views/ImagePicker.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     
     func makeUIViewController(context: Context) -> some UIViewController {
         let picker = UIImagePickerController()
@@ -17,8 +21,18 @@ struct ImagePicker: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         
     }
-    
-    func makeCoordinator() -> () {
+}
+
+extension ImagePicker {
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
         
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+        }
     }
 }
