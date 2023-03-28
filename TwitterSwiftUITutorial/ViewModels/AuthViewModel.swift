b15ff881023957462d296e22e89326e2ddfb80bf
chavalk <chavalk@hotmail.com>
2023-03-27 20:58:12 -0500

    Rearrange code to make call to store image before making call to create user.

--- TwitterSwiftUITutorial/ViewModels/AuthViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/AuthViewModel.swift
@@ -17,24 +17,24 @@ class AuthViewModel: ObservableObject {
     
     func registerUser(email: String, password: String, username: String, fullName: String, profileImage: UIImage) {
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let fileName = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(fileName)
+        
+        storageRef.putData(imageData) { _, error in
             if let error = error {
-                print("DEBUG: Error \(error.localizedDescription)")
+                print("DEBUG: Failed to upload image \(error.localizedDescription)")
                 return
             }
             
-            guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
-            let fileName = NSUUID().uuidString
-            let storageRef = Storage.storage().reference().child(fileName)
-            
-            storageRef.putData(imageData) { _, error in
-                if let error = error {
-                    print("DEBUG: Failed to upload image \(error.localizedDescription)")
-                    return
-                }
+            storageRef.downloadURL { url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
                 
-                storageRef.downloadURL { url, _ in
-                    guard let profileImageUrl = url?.absoluteString else { return }
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error \(error.localizedDescription)")
+                        return
+                    }
                 }
             }
         }
