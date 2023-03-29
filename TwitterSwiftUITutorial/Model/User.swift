dd70caa60cbcc222fd81905f62d4b05157b0fed2
chavalk <chavalk@hotmail.com>
2023-03-29 13:10:46 -0500

    Add struct for user and its properties.

--- TwitterSwiftUITutorial/Model/User.swift
+++ TwitterSwiftUITutorial/Model/User.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct User: Identifiable {
+    let id: String
+    let username: String
+    let profileImageUrl: String
+    let fullName: String
+    let email: String
+}
