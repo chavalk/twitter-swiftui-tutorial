47e7c70c2134a9a0a24556813e4bd8a31b08d16e
chavalk <chavalk@hotmail.com>
2023-03-30 18:06:39 -0500

    Add instance of profile view model and is followed binding var to profile action button view.

--- TwitterSwiftUITutorial/View/Profile/ProfileActionButtonView.swift
+++ TwitterSwiftUITutorial/View/Profile/ProfileActionButtonView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ProfileActionButtonView: View {
     let isCurrentUser: Bool
+    let viewModel: ProfileViewModel
+    @Binding var isFollowed: Bool
     
     var body: some View {
         if isCurrentUser {
