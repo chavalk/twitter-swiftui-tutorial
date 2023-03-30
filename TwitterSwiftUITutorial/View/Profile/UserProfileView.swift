08aea897e2c3b207dbb7828987fe4a2ee71166ab
chavalk <chavalk@hotmail.com>
2023-03-30 18:52:30 -0500

    Add initializer to user profile view.

--- TwitterSwiftUITutorial/View/Profile/UserProfileView.swift
+++ TwitterSwiftUITutorial/View/Profile/UserProfileView.swift
@@ -12,6 +12,11 @@ struct UserProfileView: View {
     let user: User
     @ObservedObject var viewModel: ProfileViewModel
     
+    init(user: User) {
+        self.user = user
+        self.viewModel = ProfileViewModel(user: user)
+    }
+    
     var body: some View {
         ScrollView {
             VStack {
