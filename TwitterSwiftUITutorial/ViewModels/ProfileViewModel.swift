c10997fd92892169048e0afd69c2b5389a479e8a
chavalk <chavalk@hotmail.com>
2023-03-31 08:58:45 -0500

    Add calls to database to unfollow user.

--- TwitterSwiftUITutorial/ViewModels/ProfileViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/ProfileViewModel.swift
@@ -27,6 +27,14 @@ class ProfileViewModel: ObservableObject {
     }
     
     func unfollow() {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        let followingRef = COLLECTION_FOLLOWING.document(currentUid).collection("user-following")
+        let followersRef = COLLECTION_FOLLOWERS.document(user.id).collection("user-followers")
         
+        followingRef.document(user.id).delete { _ in
+            followersRef.document(currentUid).delete { _ in
+                self.isFollowed = false
+            }
+        }
     }
 }
