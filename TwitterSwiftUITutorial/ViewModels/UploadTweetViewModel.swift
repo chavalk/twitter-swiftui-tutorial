e8c4bc2b2e1183d550c520e0b3a35f25cd7f6375
chavalk <chavalk@hotmail.com>
2023-04-03 07:31:18 -0500

    Add uid and user vars and data dictionary.

--- TwitterSwiftUITutorial/ViewModels/UploadTweetViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/UploadTweetViewModel.swift
@@ -11,6 +11,9 @@ import Firebase
 class UploadTweetViewModel: ObservableObject {
     
     func uploadTweet(caption: String) {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        guard let user = AuthViewModel.shared.user else { return }
         
+        let data: [String: Any] = ["uid": user.id, "caption": caption, "fullName": user.fullName, "timestamp": Timestamp(date: Date()), "username": user.username, "profileImageUrl": user.profileImageUrl, "likes": 0]
     }
 }
