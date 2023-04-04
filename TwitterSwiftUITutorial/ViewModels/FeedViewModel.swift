222e9b226e381ee222ceb14613785646e98fb7bd
chavalk <chavalk@hotmail.com>
2023-04-03 20:56:02 -0500

    Add call to database for tweets.

--- TwitterSwiftUITutorial/ViewModels/FeedViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/FeedViewModel.swift
@@ -15,6 +15,9 @@ class FeedViewModel: ObservableObject {
     }
     
     func fetchTweets() {
-        
+        COLLECTION_TWEETS.getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            self.tweets = documents.map({ Tweet(dictionary: $0.data()) })
+        }
     }
 }
