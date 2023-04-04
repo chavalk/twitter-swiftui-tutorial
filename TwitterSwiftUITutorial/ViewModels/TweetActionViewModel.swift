e676ef1689caa7cfda49da049c4685f9cf79378a
chavalk <chavalk@hotmail.com>
2023-04-04 13:02:29 -0500

    Add call to database to store likes.

--- TwitterSwiftUITutorial/ViewModels/TweetActionViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/TweetActionViewModel.swift
@@ -17,6 +17,16 @@ class TweetActionViewModel: ObservableObject {
     
     func likeTweet() {
         guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let tweetLikesRef = COLLECTION_TWEETS.document(tweet.id).collection("tweet-likes")
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes")
+        
+        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes + 1])
+        
+        tweetLikesRef.document(uid).setData([:]) { _ in
+            userLikesRef.document(self.tweet.id).setData([:]) { _ in
+                <#code#>
+            }
+        }
     }
     
     func unlikeTweet() {
