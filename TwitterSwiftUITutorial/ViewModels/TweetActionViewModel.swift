1e1e27211e326c5b2d21c83c4a7249179d78a3ea
chavalk <chavalk@hotmail.com>
2023-04-04 13:11:15 -0500

    Rearrange database call to make sure all three execute.

--- TwitterSwiftUITutorial/ViewModels/TweetActionViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/TweetActionViewModel.swift
@@ -21,11 +21,11 @@ class TweetActionViewModel: ObservableObject {
         let tweetLikesRef = COLLECTION_TWEETS.document(tweet.id).collection("tweet-likes")
         let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes")
         
-        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes + 1])
-        
-        tweetLikesRef.document(uid).setData([:]) { _ in
-            userLikesRef.document(self.tweet.id).setData([:]) { _ in
-                self.didLike = true
+        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes + 1]) { _ in
+            tweetLikesRef.document(uid).setData([:]) { _ in
+                userLikesRef.document(self.tweet.id).setData([:]) { _ in
+                    self.didLike = true
+                }
             }
         }
     }
