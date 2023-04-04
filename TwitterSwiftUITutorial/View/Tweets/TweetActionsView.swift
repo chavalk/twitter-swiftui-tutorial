5f1bb04c11936189ad688d14f73ff30ab5ceb34c
chavalk <chavalk@hotmail.com>
2023-04-04 08:20:26 -0500

    Add tweet actions view.

---
+++ TwitterSwiftUITutorial/View/Tweets/TweetActionsView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetActionsView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/4/23.
+//
+
+import SwiftUI
+
+struct TweetActionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetActionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetActionsView()
+    }
+}
