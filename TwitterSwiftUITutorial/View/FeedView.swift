12327f4a213f0500be75f1d30f5f273e4bc9ef9d
chavalk <chavalk@hotmail.com>
2023-04-03 20:58:59 -0500

    Remove instance of auth view model from feed view.

--- TwitterSwiftUITutorial/View/FeedView.swift
+++ TwitterSwiftUITutorial/View/FeedView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct FeedView: View {
     @State var isShowingNewTweetView = false
-    @EnvironmentObject var viewModel: AuthViewModel
+    
     
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
