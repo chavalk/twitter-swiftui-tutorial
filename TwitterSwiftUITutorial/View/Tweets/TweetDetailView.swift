3efbec1b09bf73f4dd6ec7e262a8a3b7457902d5
chavalk <chavalk@hotmail.com>
2023-04-04 08:16:57 -0500

    Add body to tweet detail view.

--- TwitterSwiftUITutorial/View/Tweets/TweetDetailView.swift
+++ TwitterSwiftUITutorial/View/Tweets/TweetDetailView.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct TweetDetailView: View {
     var body: some View {
-        Text("Tweet Detail View")
+        VStack(alignment: .leading, spacing: 16) {
+            HStack {
+                Image("batman")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(28)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Bruce Wayne")
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text("@batman")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            Text("It's not who I am underneath, but what I do that defines me.")
+                .font(.system(size: 22))
+            
+            Text("7:22 PM • 05/01/2020")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+            
+            Divider()
+            
+            HStack(spacing: 12) {
+                HStack(spacing: 4) {
+                    Text("0")
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text("Retweets")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+                
+                HStack(spacing: 4) {
+                    Text("0")
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text("Likes")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            Divider()
+        }
     }
 }
 
