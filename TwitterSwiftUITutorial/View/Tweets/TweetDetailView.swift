71cf09506326b3959c3e27026d1e9869360335c6
chavalk <chavalk@hotmail.com>
2023-04-03 21:39:46 -0500

    Add tweet detail view file.

---
+++ TwitterSwiftUITutorial/View/Tweets/TweetDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetDetailView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/3/23.
+//
+
+import SwiftUI
+
+struct TweetDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetDetailView()
+    }
+}
