9a0449ea36d6d9f7001e8a98aee4385a6af0c698
chavalk <chavalk@hotmail.com>
2023-04-04 21:29:03 -0500

    Add lazy view file.

---
+++ TwitterSwiftUITutorial/Utils/Views/LazyView.swift
@@ -0,0 +1,20 @@
+//
+//  LazyView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/4/23.
+//
+
+import SwiftUI
+
+struct LazyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LazyView_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyView()
+    }
+}
