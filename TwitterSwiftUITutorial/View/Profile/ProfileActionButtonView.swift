64c8183c36ea3ef02a289cc997f8d40deaade178
chavalk <chavalk@hotmail.com>
2023-04-05 08:29:49 -0500

    Delete is followed property from profile action button view and update references to it to call from view model.

--- TwitterSwiftUITutorial/View/Profile/ProfileActionButtonView.swift
+++ TwitterSwiftUITutorial/View/Profile/ProfileActionButtonView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct ProfileActionButtonView: View {
     let viewModel: ProfileViewModel
-    @Binding var isFollowed: Bool
     
     var body: some View {
         if viewModel.user.isCurrentUser {
@@ -25,9 +24,9 @@ struct ProfileActionButtonView: View {
         } else {
             HStack {
                 Button {
-                    isFollowed ? viewModel.unfollow() : viewModel.follow()
+                    viewModel.user.isFollowed ? viewModel.unfollow() : viewModel.follow()
                 } label: {
-                    Text(isFollowed ? "Following" : "Follow")
+                    Text(viewModel.user.isFollowed ? "Following" : "Follow")
                         .frame(width: 180, height: 40)
                         .background(Color.blue)
                         .foregroundColor(.white)
