c4d067c7b55be5f63c6ddd99137b9288551c0c75
chavalk <chavalk@hotmail.com>
2023-04-05 17:12:06 -0500

    Add user property to conversations view.

--- TwitterSwiftUITutorial/View/Messages/ConversationsView.swift
+++ TwitterSwiftUITutorial/View/Messages/ConversationsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ConversationsView: View {
     @State var isShowingNewMessageView = false
     @State var showChat = false
+    @State var user: User?
     @ObservedObject var viewModel = ConversationsViewModel()
     
     var body: some View {
