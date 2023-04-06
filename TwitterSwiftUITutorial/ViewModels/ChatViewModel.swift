91e851d00cbc8eba861f68f902c271e9c19786a3
chavalk <chavalk@hotmail.com>
2023-04-05 19:08:11 -0500

    Paste code for fetch messages function from fetch recent messages function in conversations model.

--- TwitterSwiftUITutorial/ViewModels/ChatViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/ChatViewModel.swift
@@ -16,7 +16,24 @@ struct ChatViewModel {
     }
     
     func fetchMessages() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
         
+        let query = COLLECTION_MESSAGES.document(uid).collection("recent-messages")
+        query.order(by: "timestamp", descending: true)
+        
+        query.addSnapshotListener { snapshot, error in
+            guard let changes = snapshot?.documentChanges else { return }
+            
+            changes.forEach { change in
+                let messageData = change.document.data()
+                let uid = change.document.documentID
+                
+                COLLECTION_USERS.document(uid).getDocument { snapshot, _ in
+                    guard let data = snapshot?.data() else { return }
+                    let user = User(dictionary: data)
+                }
+            }
+        }
     }
     
     func sendMessage(_ messageText: String) {
