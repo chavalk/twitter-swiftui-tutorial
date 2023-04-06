797ce087a6b1a037860c92c20c35ed7408cefdf9
chavalk <chavalk@hotmail.com>
2023-04-05 19:23:12 -0500

    Modify database calls to get correct data for fetch messages function.

--- TwitterSwiftUITutorial/ViewModels/ChatViewModel.swift
+++ TwitterSwiftUITutorial/ViewModels/ChatViewModel.swift
@@ -19,19 +19,20 @@ class ChatViewModel: ObservableObject {
     func fetchMessages() {
         guard let uid = AuthViewModel.shared.userSession?.uid else { return }
         
-        let query = COLLECTION_MESSAGES.document(uid).collection("recent-messages")
+        let query = COLLECTION_MESSAGES.document(uid).collection(user.id)
         query.order(by: "timestamp", descending: true)
         
         query.addSnapshotListener { snapshot, error in
-            guard let changes = snapshot?.documentChanges else { return }
+            guard let changes = snapshot?.documentChanges.filter({ $0.type == .added }) else { return }
             
             changes.forEach { change in
                 let messageData = change.document.data()
-                let uid = change.document.documentID
+                guard let fromId = messageData["fromId"] as? String else { return }
                 
-                COLLECTION_USERS.document(uid).getDocument { snapshot, _ in
+                COLLECTION_USERS.document(fromId).getDocument { snapshot, _ in
                     guard let data = snapshot?.data() else { return }
                     let user = User(dictionary: data)
+                    self.messages.append(Message(user: user, dictionary: messageData))
                 }
             }
         }
