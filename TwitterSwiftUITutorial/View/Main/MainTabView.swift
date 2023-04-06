d9afeda410eff6c80d59e216e84559c35d5f2747
chavalk <chavalk@hotmail.com>
2023-04-05 21:43:06 -0500

    Add main tab view.

---
+++ TwitterSwiftUITutorial/View/Main/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by Jose Garcia on 4/5/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
